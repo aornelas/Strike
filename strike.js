11096f384146b185e05c8b4279c08501252da4d1
Andres Ornelas <andresornelas@gmail.com>
2012-07-07 18:47:20 -0700

    Make use of Array.forEach to loop through tweet action rows
    Per phiveleven's review

--- strike.js
+++ strike.js
@@ -1,21 +1,19 @@
-tweetActionRows = document.getElementsByClassName('tweet-actions')
+Array.prototype.slice.call(document.querySelectorAll('ul.tweet-actions')).forEach(function(ul) {
+    var i = document.createElement('i')
+    i.class = 'sm-strike'
 
-for(var tweet = 0; tweet < tweetActionRows.length; tweet++) {
-	var i = document.createElement('i')
-	i.class = 'sm-strike'
-	
-	var b = document.createElement('b')
-	b.textContent = '✘ Strike'
-	
-	var a = document.createElement('a')
-	a.class = 'with-icn js-toggle-strike'
-	a.href = '#'
-	a.appendChild(i)
-	a.appendChild(b)
-	
-	var li = document.createElement('li')
-	li.class = 'action-strike'
-	li.appendChild(a)
-	
-	tweetActionRows[tweet].appendChild(li)
-}
+    var b = document.createElement('b')
+    b.textContent = '✘ Strike'
+
+    var a = document.createElement('a')
+    a.class = 'with-icn js-toggle-strike'
+    a.href = '#'
+    a.appendChild(i)
+    a.appendChild(b)
+
+    var li = document.createElement('li')
+    li.class = 'action-strike'
+    li.appendChild(a)
+
+    ul.appendChild(li)
+})
