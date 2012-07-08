f82dd31230bc49c0eab4dbe50db34a6f53716013
Andres Ornelas <andresornelas@gmail.com>
2012-07-07 19:28:45 -0700

    Refactor for readability
    - Extract injectStrikes()
    - Extend NodeList with Array's forEach

--- strike.js
+++ strike.js
@@ -1,19 +1,28 @@
-Array.prototype.slice.call(document.querySelectorAll('ul.tweet-actions')).forEach(function(ul) {
-    var i = document.createElement('i')
-    i.class = 'sm-strike'
 
-    var b = document.createElement('b')
-    b.textContent = '✘ Strike'
+// Extend NodeList with Array's forEach
+NodeList.prototype.forEach = Array.prototype.slice.call(this).forEach
 
-    var a = document.createElement('a')
-    a.class = 'with-icn js-toggle-strike'
-    a.href = '#'
-    a.appendChild(i)
-    a.appendChild(b)
+function injectStrikes() {
+    document.querySelectorAll('ul.tweet-actions') // returns NodeList
+            .forEach(function(ul) {
+        var i = document.createElement('i')
+        i.class = 'sm-strike'
 
-    var li = document.createElement('li')
-    li.class = 'action-strike'
-    li.appendChild(a)
+        var b = document.createElement('b')
+        b.textContent = '✘ Strike'
 
-    ul.appendChild(li)
-})
+        var a = document.createElement('a')
+        a.class = 'with-icn js-toggle-strike'
+        a.href = '#'
+        a.appendChild(i)
+        a.appendChild(b)
+
+        var li = document.createElement('li')
+        li.class = 'action-strike'
+        li.appendChild(a)
+
+        ul.appendChild(li)
+    })
+}
+
+injectStrikes()
